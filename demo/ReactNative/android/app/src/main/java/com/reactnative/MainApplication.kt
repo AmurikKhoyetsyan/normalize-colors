b480045c9a3a5381da5d38c95906951b06e5849d
AmurKhoyetsyan <javanewss@gmail.com>
2026-03-06 16:48:53 +0400

    replace react-native

--- demo/ReactNative/android/app/src/main/java/com/reactnative/MainApplication.kt
+++ demo/ReactNative/android/app/src/main/java/com/reactnative/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.demoreactnative
+package com.reactnative
 
 import android.app.Application
 import com.facebook.react.PackageList
--- demo/ReactNative/ios/ReactNative/AppDelegate.swift
+++ demo/ReactNative/ios/ReactNative/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "DemoReactNative",
+      withModuleName: "ReactNative",
       in: window,
       launchOptions: launchOptions
     )
